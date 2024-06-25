d0d9f6e0d2fbc74c7058784418db60a3351d1888
Sonu Kumar <thesonukumar357@gmail.com>
2024-06-25 08:16:48 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import "./App.css";
+import { useEffect, useMemo, useState } from "react";
+import Weather from "./Weather";
+
+function App() {
+
+  
+  return (
+    <div className="todo-container">
+      <Weather/>
+    </div>   
+  );
+  }
+export default App;
